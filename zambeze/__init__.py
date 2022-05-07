b946315f052bd24c8e269d8e6e617775e8ad2856
Rafael Ferreira da Silva <silvarf@ornl.gov>
2022-05-07 00:01:55 -0400

    repository configuration

--- zambeze/__init__.py
+++ zambeze/__init__.py
@@ -6,6 +6,8 @@
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the MIT License.
 
+from .version import __version__
+
 __author__ = "https://zambeze.org"
 __credits__ = "Oak Ridge National Laboratory"
 
---
+++ zambeze/campaign.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2022 Oak Ridge National Laboratory.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the MIT License.
+
+import logging
+
+from typing import Optional
+
+
+class Campaign:
+    """A Science Campaign
+
+    :param name: Science campaign name.
+    :type name: str
+    :param logger: The logger where to log information/warning or errors.
+    :type logger: Optional[logging.Logger]
+    """
+
+    def __init__(self, name: str, logger: Optional[logging.Logger] = None) -> None:
+        """Create an object that represents a science campaign."""
+        self.logger: logging.Logger = (
+            logging.getLogger(__name__) if logger is None else logger
+        )
+        self.name = name
