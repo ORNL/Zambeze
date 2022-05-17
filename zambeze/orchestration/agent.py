108edc65b7be3a78cc368f9da5cbfc8271ddf676
Ketan Maheshwari <ketancmaheshwari@gmail.com>
2022-05-17 08:35:12 -0400

    a simple working version

--- zambeze/orchestration/agent.py
+++ zambeze/orchestration/agent.py
@@ -6,11 +6,8 @@ async def main():
     nc = await nats.connect("172.22.1.67")
     print("Connected")
     sub = await nc.subscribe("execapp")
-    while True:
-        msg = await sub.next_msg()
-        handle(msg)
-        print("Message handles, going to sleep for 10s")
-        time.sleep(10)
+    msg = await sub.next_msg(timeout=100)
+    await handle(msg)
 
 
 async def handle(msg):
