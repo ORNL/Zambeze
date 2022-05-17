fc15994f06aae9069e6035d95377ed37871a55e0
Ketan Maheshwari <ketancmaheshwari@gmail.com>
2022-05-17 08:18:51 -0400

    adding client and agent

---
+++ zambeze/campaign/client.py
@@ -0,0 +1,14 @@
+import asyncio
+import nats
+
+async def main():
+    nc = await nats.connect("172.22.1.67")
+
+    await nc.publish("execapp", b"/bin/echo hello world.")
+
+    await nc.close()
+
+
+if __name__ == "__main__":
+   asyncio.run(main())
+
--- zambeze/orchestration/agent.py
+++ zambeze/orchestration/agent.py
@@ -1,11 +1,27 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-#
-# Copyright (c) 2022 Oak Ridge National Laboratory.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the MIT License.
-
 import asyncio
 import nats
-from nats.errors import TimeoutError
+import time
+
+async def main():
+    nc = await nats.connect("172.22.1.67")
+    print("Connected")
+    sub = await nc.subscribe("execapp")
+    while True:
+        msg = await sub.next_msg()
+        handle(msg)
+        print("Message handles, going to sleep for 10s")
+        time.sleep(10)
+
+
+async def handle(msg):
+
+    knownmsg=["execapp","datatransfer"]
+    
+    if msg in knownmsg:
+        print(msg)
+    else:
+        print("Unknown message")
+
+
+if __name__=="__main__":
+   asyncio.run(main())
