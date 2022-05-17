19810fdcf4762aacfad73e4c1a55145374b74d88
Ketan Maheshwari <ketancmaheshwari@gmail.com>
2022-05-17 09:50:56 -0400

    a few changes

--- zambeze/orchestration/agent.py
+++ zambeze/orchestration/agent.py
@@ -4,21 +4,14 @@ import time
 
 async def main():
     nc = await nats.connect("172.22.1.67")
-    print("Connected")
-    sub = await nc.subscribe("execapp")
-    msg = await sub.next_msg(timeout=100)
-    await handle(msg)
-
-
-async def handle(msg):
-
     knownmsg=["execapp","datatransfer"]
-    
-    if msg.subject in knownmsg:
-        print(msg)
-    else:
-        print("Unknown message")
-
+    async def handle(msg):
+        if msg.subject in knownmsg:
+            print(msg)
+        else:
+            print("Unknown message")
+    sub = await nc.subscribe("execapp", cb=handle)
+    msg = await sub.next_msg(timeout=100)
 
 if __name__=="__main__":
    asyncio.run(main())
