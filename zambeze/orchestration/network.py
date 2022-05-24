48eb0de4eb87fa15b27edc7770e96572e4acddf4
JoshuaSBrown <joshbro42867@yahoo.com>
2022-05-24 09:38:25 -0400

    Clean up network file

--- zambeze/orchestration/network.py
+++ zambeze/orchestration/network.py
@@ -1,8 +1,12 @@
 import http.client as httplib
 
-def externalNetworkConnectionDetected():
-    """Check if we can connect to external network. 8.8.8.8 is the 
-    Google namespace server."""
+def externalNetworkConnectionDetected() -> bool:
+    """Check if we can connect to external network.
+
+    :returns: True if can connect to google server, false otherwise
+    :rtype: bool 
+    Using the ip address 8.8.8.8 which is the Google namespace server.
+    """
     connection = httplib.HTTPSConnection("8.8.8.8", timeout=5)
     try:
         connection.request("HEAD", "/")
