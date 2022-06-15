90ce2086369ec01d70b9d2a852e1a495afbecdd5
Rafael Ferreira da Silva <silvarf@ornl.gov>
2022-06-15 18:59:43 -0400

    load settings properties for services

--- zambeze/settings.py
+++ zambeze/settings.py
@@ -9,7 +9,7 @@
 import pathlib
 import yaml
 
-from typing import Union
+from typing import Dict, List, Union
 
 
 class ZambezeSettings:
@@ -38,9 +38,10 @@ class ZambezeSettings:
 
         # set default values
         if not self.settings:
-            self.settings = {"nats": {}}
+            self.settings = {"nats": {}, "services": {}}
         self.__set_default("host", "localhost", self.settings["nats"])
         self.__set_default("port", 4222, self.settings["nats"])
+        self.__set_default("services", {}, self.settings)
         self.__save()
 
     def get_nats_connection_uri(self) -> str:
@@ -51,6 +52,34 @@ class ZambezeSettings:
         :rtype: str
         """
         return f"nats://{self.settings['nats']['host']}:{self.settings['nats']['port']}"
+
+    def get_service_names(self) -> List[str]:
+        """
+        Get a list of service names.
+
+        :return: List of service names
+        :rtype: List[str]
+        """
+        services = []
+        for service in self.settings["services"]:
+            services.append(service["name"].lower())
+
+    def get_service_properties(
+        self, service_name: str
+    ) -> Dict[str, Union[str, int, float, List]]:
+        """
+        Get a List of properties from a service.
+
+        :param service_name: Service name
+        :type service_name: str
+
+        :return: List of properties
+        :rtype: Dict[str, Union[str, int, float, List]]
+        """
+        for service in self.settings["services"]:
+            if service["name"] == service_name:
+                return service["properties"]
+        return None
 
     def __set_default(
         self, key: str, value: Union[int, float, str, dict], base: dict
