30571a23464681aab90f2e4bc92656b15576c0c0
JoshuaSBrown <brownjs@ornl.gov>
2022-11-09 10:03:44 -0500

    Fix race condition in Globus

--- tests/test_plugin_globus.py
+++ tests/test_plugin_globus.py
@@ -310,7 +310,7 @@ def test_globus_process():
     globus_plugin.configure(configuration)
 
     # Create a file on the local posix system
-    file_name = "demofile_for_globus1.txt"
+    file_name = "demofile_for_globus1-" + str(time.time_ns()) + ".txt"
     f = open(file_name, "w")
     original_number = random.randint(0, 100000000000)
     f.write(str(original_number))
