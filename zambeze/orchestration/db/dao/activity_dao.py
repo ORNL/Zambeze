d93e2af965169b8a0743a1322d4c2da1bdbe9442
JoshuaSBrown <joshbro42867@yahoo.com>
2022-11-28 21:36:00 -0500

    Attempt to fix inconsistent methods again

--- zambeze/orchestration/db/dao/activity_dao.py
+++ zambeze/orchestration/db/dao/activity_dao.py
@@ -4,10 +4,10 @@ from zambeze.orchestration.db.model.abstract_entity import AbstractEntity
 
 
 class ActivityDAO(AbstractDAO):
-    def insert(self, activity: AbstractEntity) -> None:
-        values = activity.get_all_values()
-        insert_stmt = get_insert_stmt(activity)
-        self._logger.debug(f"Saving activity: {insert_stmt}")
+    def insert(self, entity: AbstractEntity) -> None:
+        values = entity.get_all_values()
+        insert_stmt = get_insert_stmt(entity)
+        self._logger.debug(f"Saving entity: {insert_stmt}")
         self._logger.debug(f"\t Values: {values}")
         try:
             self._engine.execute(insert_stmt, values)
@@ -16,10 +16,10 @@ class ActivityDAO(AbstractDAO):
             self._logger.error(error_msg)
             raise Exception(error_msg)
 
-    def insert_and_return_id(self, activity: AbstractEntity) -> int:
-        values = activity.get_all_values()
-        insert_stmt = get_insert_stmt(activity)
-        self._logger.debug(f"Saving activity: {insert_stmt}")
+    def insert_and_return_id(self, entity: AbstractEntity) -> int:
+        values = entity.get_all_values()
+        insert_stmt = get_insert_stmt(entity)
+        self._logger.debug(f"Saving entity: {insert_stmt}")
         self._logger.debug(f"\t Values: {values}")
         try:
             conn = self._engine.connect()
@@ -34,11 +34,11 @@ class ActivityDAO(AbstractDAO):
             self._logger.error(error_msg)
             raise Exception(error_msg)
 
-    def update(self, activity: AbstractEntity) -> None:
-        update_stmt = get_update_stmt(activity)
+    def update(self, entity: AbstractEntity) -> None:
+        update_stmt = get_update_stmt(entity)
         try:
             conn = self._engine.connect()
-            conn.execute(update_stmt, activity.get_values_without_id())
+            conn.execute(update_stmt, entity.get_values_without_id())
         except Exception as e:
             error_msg = f"Error while updating in the local db. The exception was\n {e}"
             self._logger.error(error_msg)
