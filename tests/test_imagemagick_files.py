26140845db3419e611c74d5c4d16e5a61a64523f
Renan Souza <contact@renansouza.org>
2023-01-10 15:55:02 -0500

    Calling examples/imagemagick_files.py.main() in integration test

--- tests/test_imagemagick_files.py
+++ tests/test_imagemagick_files.py
@@ -9,48 +9,7 @@ import os
 
 from zambeze import Campaign, ShellActivity
 
-
-def image_magic_main():
-
-    # logging (for debugging purposes)
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-    formatter = logging.Formatter(
-        "[Zambeze Agent] [%(levelname)s] %(asctime)s - %(message)s"
-    )
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
-
-    # create campaign
-    campaign = Campaign("My ImageMagick Campaign", logger=logger)
-
-    # define an activity
-    curr_dir = os.path.dirname(__file__)
-    activity = ShellActivity(
-        name="ImageMagick",
-        files=[
-            f"file://{curr_dir}/../tests/campaigns/imagesequence/{i:02d}.jpg"
-            for i in range(1, 11)
-        ],
-        command="convert",
-        arguments=[
-            "-delay",
-            "20",
-            "-loop",
-            "0",
-            f"{curr_dir}/../tests/campaigns/imagesequence/*.jpg",
-            "a.gif",
-        ],
-        logger=logger,
-        # Uncomment if running on M1 Mac.
-        env_vars=[("PATH", "$PATH:/opt/homebrew/bin")],
-    )
-    campaign.add_activity(activity)
-
-    # run the campaign
-    campaign.dispatch()
+from examples.imagemagick_files import main
 
 
 @pytest.mark.integration
@@ -75,7 +34,6 @@ def test_imagemagick_files():
     if os.path.exists(final_file_path):
         os.remove(final_file_path)
 
-    image_magic_main()
     # command = ['python3', image_magick_example]
     #
     # # Step 4 launch example
@@ -88,6 +46,7 @@ def test_imagemagick_files():
     # stdout, stderr = process.communicate()
     # print(stdout)
     # print(stderr)
+    main()
 
     # Step 5 wait for example to complete
     count = 0
