5f76f1e8dc60b39e92c640fb746b521f692084bd
tskluzac <skluzacek@uchicago.edu>
2023-01-10 20:30:33 -0600

    update message activity.

---
+++ zambeze/orchestration/message/activity_message/message_activity.py
@@ -0,0 +1,16 @@
+import logging
+from ..abstract_message import AbstractMessage
+from zambeze.orchestration.zambeze_types import MessageType
+from typing import Optional
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class MessageActivity(AbstractMessage):
+    type = MessageType.ACTIVITY
+    data = {}
+
+    def __init__(self, new_data: dict, logger: Optional[logging.Logger] = None) -> None:
+        super().__init__()
+        object.__setattr__(self, "type", MessageType.ACTIVITY)
+        object.__setattr__(self, "data", new_data)
