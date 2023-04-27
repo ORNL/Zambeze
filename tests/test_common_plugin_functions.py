cbfefcf79d1217d62e9d7fa427c1756d8c4d4f68
Brown, Joshua <brownjs@ornl.gov>
2023-04-27 15:43:24 -0400

    Fix module import issue

---
+++ tests/test_common_plugin_functions.py
@@ -0,0 +1,23 @@
+# Local imports
+from zambeze.orchestration.plugin_modules.common_plugin_functions import (
+    registerPlugins
+)
+
+# Standard imports
+import os
+import pytest
+import uuid
+
+
+@pytest.mark.unit
+def test_registerPlugins():
+
+    plugins = registerPlugins()
+    assert len(plugins) > 0
+
+    for plugin in plugins:
+        # Lets make sure we don't import system plugins i.e. git.cmd we will
+        # do this by assuming we should only be importing plugins that are flat
+        # and are not nested 'git' is valid 'git.something' is not
+        assert '.' not in plugin
+
--- zambeze/orchestration/plugin_modules/common_plugin_functions.py
+++ zambeze/orchestration/plugin_modules/common_plugin_functions.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from typing import Optional
 
 import logging
+import os
 import pkgutil
 
 
@@ -16,7 +17,8 @@ def registerPlugins(logger: Optional[logging.Logger] = None) -> list:
     module_names = []
     # plugin_path = [str(Path(__file__).resolve().parent) + "/plugin_modules"]
     plugin_path = [str(Path(__file__).resolve().parent)]
-    for importer, module_name, ispkg in pkgutil.walk_packages(path=plugin_path):
+    print(plugin_path)
+    for importer, module_name, ispkg in pkgutil.walk_packages(plugin_path):
         if (
             module_name != "abstract_plugin"
             and module_name != "__init__"
@@ -27,7 +29,13 @@ def registerPlugins(logger: Optional[logging.Logger] = None) -> list:
             and module_name != "abstract_uri_separator"
             and module_name != "file_uri_separator"
         ):
-            module_names.append(module_name)
+
+            module_path = module_name.replace('.', os.path.sep)
+            full_module_path = os.path.join(plugin_path[0], module_path)
+            # Make sure we are only grabbing zambeze plugins and not modules
+            # from elsewhere in the Python path
+            if os.path.isdir(full_module_path):
+                module_names.append(module_name)
     if logger:
         logger.debug(f"Registered Plugins: {', '.join(module_names)}")
     return module_names
