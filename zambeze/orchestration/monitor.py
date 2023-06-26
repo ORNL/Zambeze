4d96b16feecdea5f658689a52f38d1db6d5102b2
tskluzac <skluzacek@uchicago.edu>
2023-06-26 13:31:37 -0500

    use correct heartbeat incrementing.

--- zambeze/orchestration/monitor.py
+++ zambeze/orchestration/monitor.py
@@ -1,6 +1,6 @@
-
 import threading
 
+from time import time
 from queue import Queue
 
 
@@ -8,19 +8,20 @@ class Monitor(threading.Thread):
     def __init__(self, dag_msg, logger):
         threading.Thread.__init__(self)
         self.to_monitor_q = Queue()
+        self.to_status_q = Queue()
         self.completed = False
 
         # Now we want to hold (and periodically log) until all subtasks are complete.
         self.dag_dict = dict()
         self.dag_msg = dag_msg
 
+        self.monitor_hb_s = 5
+
         self._logger = logger
 
     def run(self):
         """Override the Thread 'run' method to instead run our
         process when Thread.start() is called!"""
-        # Create persisent "__process()"
-        # self.__process()
 
         self._logger.info("[MONITOR] ping from monitor.py thread! ")
         # Add all activities (besides the monitor) to our dict.
@@ -28,15 +29,39 @@ class Monitor(threading.Thread):
             if activity_id == "MONITOR":
                 continue
             self.dag_dict[activity_id] = "PROCESSING"
+            self._logger.info("[monitor] Marked all campaign activities for monitoring!")
 
+        last_hb_time = time()
         while True:
+            # self._logger.info("[monitor] QQQ")
             # Quick check to see if all values are NOT "PROCESSING"
-            proc_count = sum(x == 'PROCESSING' for x in self.dag_dict.values())
+            proc_count = sum(x == "PROCESSING" for x in self.dag_dict.values())
 
+            # self._logger.info("[monitor] RRR")
             if proc_count == 0:
                 self.completed = True
 
-            status_msg = self.to_monitor_q.get()
-            if status_msg.activity_id in self.dag_dict:
-                status = status_msg["status"]
-                self.dag_dict[status_msg['activity_id']] = status
+            # self._logger.info("[monitor] SSS")
+
+            if self.to_monitor_q.qsize() > 0:
+                status_msg = self.to_monitor_q.get()
+                # self._logger.info("[monitor] TTT")
+                if status_msg.activity_id in self.dag_dict:
+                    status = status_msg["status"]
+                    self.dag_dict[status_msg["activity_id"]] = status
+
+            # If enough time has passed, hb...
+            if time() - last_hb_time > self.monitor_hb_s:
+                # Send heartbeat and sleep.
+                hb_monitor_msg = {
+                        'status': "MONITORING",
+                        'activity_id': "MONITORING",
+                        'campaign_id': "CAMPAIGN ABC TODO",
+                        'msg': 'simple heartbeat notification.'
+                    }
+
+                self.to_status_q.put(hb_monitor_msg)
+                self._logger.info("[monitor] Enqueued monitor hb message! ")
+                last_hb_time = time()
+
+            # TODO: IF EVERYTHING IS DONE, TERMINATE BY SETTING EXECUTOR.MONITOR TO "NONE".
