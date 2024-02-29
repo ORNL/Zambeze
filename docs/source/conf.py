2347280d5aacbc16d149c0f0a046403e37463763
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2024-02-29 09:37:17 -0500

    Fix import order in docs config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,9 +12,10 @@
 
 import os
 import sys
-import zambeze
 
 sys.path.insert(0, os.path.abspath("../.."))
+
+import zambeze
 
 # -- Project information -----------------------------------------------------
 
