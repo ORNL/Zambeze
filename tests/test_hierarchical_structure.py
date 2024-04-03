62fc84a6f3b4e4ac8ab682c8497f260e14463279
tskluzac <skluzacek@uchicago.edu>
2024-04-03 09:14:47 -0500

    [working] trying to get black to pass.

--- tests/test_hierarchical_structure.py
+++ tests/test_hierarchical_structure.py
@@ -6,7 +6,7 @@ from zambeze.campaign.activities.abstract_activity import Activity
 @pytest.mark.unit
 def test_shell_activity_is_activity():
     activity = ShellActivity(
-        name="Simple activity ", files=[], command="echo", arguments=["hello"],
+        name="Simple activity ", files=[], command="echo", arguments=["hello"]
     )
 
     assert isinstance(activity, ShellActivity)
