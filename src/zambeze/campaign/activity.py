a16e5c3b291310d37f2499236fd2b1d81965b94b
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2024-07-12 13:36:21 -0400

    Remove the ActivityStatus enum and clean up protocol

--- src/zambeze/campaign/activity.py
+++ src/zambeze/campaign/activity.py
@@ -3,36 +3,13 @@
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the MIT License.
 
-import logging
-from enum import Enum
-from typing import Optional, Protocol
+from typing import Protocol
 from zambeze.orchestration.message.abstract_message import AbstractMessage
-
-
-class ActivityStatus(Enum):
-    CREATED = 1
-    QUEUED = 2
-    RUNNING = 3
-    COMPLETED = 4
-    FAILED = 5
 
 
 class Activity(Protocol):
     """Protocol for campaign activities."""
 
     name: str
-    files: list[str]
-    command: Optional[str]
-    arguments: list[str]
-    logger: Optional[logging.Logger]
-    campaign_id: Optional[str]
-    origin_agent_id: Optional[str]
-    running_agent_ids: Optional[list[str]]
-    message_id: Optional[str]
-    activity_id: Optional[str]
-    source_file: Optional[str]
-    dest_directory: Optional[str]
-    override_existing: Optional[bool]
-    submission_time: Optional[str]
 
     def generate_message(self) -> AbstractMessage: ...
